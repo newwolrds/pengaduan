30d9f0735099d9aca5d6fe1f7d914f3c591cf83d
newwolrds <newwolrds09@gmail.com>
2023-04-20 15:15:57 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -32,7 +32,7 @@ class HomeController extends Controller
     {
         $response = Http::withHeaders([
             'Authorization' => 'eyJpdiI6Ikc0RHdiOUF0TU9IQWpoVVR3SVFNU0E9PSIsInZhbHVlIjoiN2pEd3ZObmZTYStYdVhzL1NFUGlldz09IiwibWFjIjoiMTVkMGQyYTQxYWRmM2IzZWY4OWVmZTUyOGUwNGFmY2E1MzAwZTBlYTIwNjI5ODRkODVhYTQyZjIyYTgwMWJkZSIsInRhZyI6IiJ9YspAJbi4jljFFRy',
-        ])->get('https://apiext-dev.id-trec.com//api/v1/principals');
+        ])->get('https://apiext-dev.id-trec.com/api/v1/principals');
 
         if ($response->successful()) {
             return response()->json([
