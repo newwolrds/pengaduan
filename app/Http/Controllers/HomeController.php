0fe8486f196359fe33e04530e9a43d4dc5877d1a
newwolrds <newwolrds09@gmail.com>
2023-04-20 15:02:03 +0700

    update

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -31,7 +31,7 @@ class HomeController extends Controller
     public function getData(Request $request)
     {
         $response = Http::withHeaders([
-            'Authorization' => 'eyJpdiI6IkRHalU3UlYwQkV4SHlJcC9FWHdqSVE9PSIsInZhbHVlIjoicGxISWRFQVBOM2NyWG9VTVpQZGRldz09IiwibWFjIjoiODk4NzhhNGQ5ODljOGVjZjI3ZDk0ZWEzZmQwMjZjYWI2OWUyNjMxNDJjN2IzMzAwMWMxMDc1NDNhNzNmN2MyMSIsInRhZyI6IiJ98vmZTMbBqDmVNlb',
+            'Authorization' => 'eyJpdiI6Ikc0RHdiOUF0TU9IQWpoVVR3SVFNU0E9PSIsInZhbHVlIjoiN2pEd3ZObmZTYStYdVhzL1NFUGlldz09IiwibWFjIjoiMTVkMGQyYTQxYWRmM2IzZWY4OWVmZTUyOGUwNGFmY2E1MzAwZTBlYTIwNjI5ODRkODVhYTQyZjIyYTgwMWJkZSIsInRhZyI6IiJ9YspAJbi4jljFFRy',
         ])->get('https://apiext-dev.id-trec.com//api/v1/principals');
 
         if ($response->successful()) {
